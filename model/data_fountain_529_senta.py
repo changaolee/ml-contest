640e0cb0210b48ec17eb9d729d523d3d65a907da
lichangao1826 <lca1826@163.com>
2021-10-27 00:32:35 +0800

    up

--- model/data_fountain_529_senta.py
+++ model/data_fountain_529_senta.py
@@ -1,5 +1,5 @@
 from bunch import Bunch
-from paddle import nn, unsqueeze
+from paddle import nn
 from paddlenlp.transformers import BertPretrainedModel, SkepPretrainedModel
 
 
@@ -15,17 +15,8 @@ class DataFountain529SentaBertBaselineModel(BertPretrainedModel):
         self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = nn.layer.Linear(self.config.hidden_size, self.config.num_classes)
 
-    def forward(self, input_ids, token_type_ids=None, position_ids=None, attention_mask=None):
-        # 将 attention_mask 进行维度变换，从 2 维变成 4 维
-        # paddlenlp.transformers 的实现与 torch 或 tf 不一样，不会自动进行维度扩充
-        attention_mask = unsqueeze(attention_mask, axis=[1, 2])
-
-        _, pooled_output = self.bert(
-            input_ids,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            attention_mask=attention_mask)
-
+    def forward(self, input_ids):
+        _, pooled_output = self.bert(input_ids)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         return logits
@@ -43,17 +34,8 @@ class DataFountain529SentaSkepBaselineModel(SkepPretrainedModel):
         self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
         self.classifier = nn.layer.Linear(self.config.hidden_size, self.config.num_classes)
 
-    def forward(self, input_ids, token_type_ids=None, position_ids=None, attention_mask=None):
-        # 将 attention_mask 进行维度变换，从 2 维变成 4 维
-        # paddlenlp.transformers 的实现与 torch 或 tf 不一样，不会自动进行维度扩充
-        attention_mask = unsqueeze(attention_mask, axis=[1, 2])
-
-        _, pooled_output = self.skep(
-            input_ids,
-            token_type_ids=token_type_ids,
-            position_ids=position_ids,
-            attention_mask=attention_mask)
-
+    def forward(self, input_ids):
+        _, pooled_output = self.skep(input_ids)
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         return logits
