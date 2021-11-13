e6df7403d0ba5d40dea17c1f9c11c96a27e00831
lichangao1826 <lca1826@163.com>
2021-11-13 22:17:22 +0800

    bert hidden fusion

--- model/data_fountain_529_senta.py
+++ model/data_fountain_529_senta.py
@@ -1,26 +1,29 @@
+import paddle
 from bunch import Bunch
-from paddle import nn
 from paddlenlp.transformers import BertPretrainedModel
 
 
 class DataFountain529SentaBertHiddenFusionModel(BertPretrainedModel):
     """
-    df-529 情感分类模型 baseline: bert + fc
+    Bert 隐藏层向量动态融合
     """
 
     def __init__(self, bert, config: Bunch):
         super().__init__()
         self.config = config
         self.bert = bert
-        self.fusion_linear = nn.Linear(12, 768)
-        self.dropout = nn.Dropout(self.config.hidden_dropout_prob)
-        self.classifier = nn.layer.Linear(self.config.hidden_size, self.config.num_classes)
+        self.dropout = paddle.nn.Dropout(self.config.hidden_dropout_prob)
+        self.classifier = paddle.nn.layer.Linear(self.config.hidden_size, self.config.num_classes)
+        self.layer_weights = self.create_parameter(shape=(12, 1, 1),
+                                                   default_initializer=paddle.nn.initializer.Constant(1.0))
 
     def forward(self, input_ids, token_type_ids):
         encoder_outputs, _ = self.bert(input_ids,
                                        token_type_ids=token_type_ids,
                                        output_hidden_states=True)
-        pooled_output = self.fusion_linear(encoder_outputs)
-        pooled_output = self.dropout(pooled_output)
+        stacked_encoder_outputs = paddle.stack(encoder_outputs, axis=0)
+        all_layer_cls_embedding = stacked_encoder_outputs[:, :, 0, :]
+        weighted_average = (self.layer_weights * all_layer_cls_embedding).sum(axis=0) / self.layer_weights.sum()
+        pooled_output = self.dropout(weighted_average)
         logits = self.classifier(pooled_output)
         return logits
--- run/data_fountain_529_senta/predict.py
+++ run/data_fountain_529_senta/predict.py
@@ -22,16 +22,16 @@ def predict():
 
     k_fold_result = []
     k_fold_models = {
-        1: "model_400",
-        2: "model_400",
-        3: "model_424",
-        4: "model_400",
-        5: "model_424",
-        6: "model_424",
-        7: "model_300",
-        8: "model_424",
-        9: "model_400",
-        10: "model_424"
+        1: "model_200",
+        2: "model_200",
+        3: "model_318",
+        4: "model_318",
+        5: "model_200",
+        6: "model_300",
+        7: "model_318",
+        8: "model_300",
+        9: "model_200",
+        10: "model_300"
     }
     for fold, model_path in k_fold_models.items():
         # 获取测试集
