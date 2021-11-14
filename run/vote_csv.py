465eca99f9e70829e157979f308d619763d1eea6
lichangao1826 <lca1826@163.com>
2021-11-14 21:48:57 +0800

    up

---
+++ run/vote_csv.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import csv
+
+results = {'bert_hidden_fusion.csv': 0.69647375626,
+           'roberta_hidden_fusion.csv': 0.66096826005,
+           'skep_base.csv': 0.67728485854}
+
+dfs = [(pd.read_csv(filename), weight) for filename, weight in results.items()]
+
+results = []
+
+num = len(dfs[0][0])
+for i in range(num):
+    vote = {}
+    _id = dfs[0][0].iloc[i]['id']
+    for j in range(len(dfs)):
+        df, weight = dfs[j]
+        label = df.iloc[i]['class']
+        vote[label] = vote.get(label, 0.) + weight
+    label = sorted(vote, key=lambda x: vote[x])[-1]
+    results.append([_id, label])
+
+with open("result.csv", "w", encoding="utf-8") as f:
+    writer = csv.writer(f)
+    writer.writerow(["id", "class"])
+    for line in results:
+        qid, label = line
+        writer.writerow([qid, label])
